ae8e3c84043852f76215ae82652f3fb60e3c91f0
Will Mason <willmason@live.com>
2022-07-01 16:10:02 +0100

    Update navigation remove .php on all links

--- sections/navigation.php
+++ sections/navigation.php
@@ -23,22 +23,22 @@ $decorative = 6;
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav me-auto p-3 p-lg-0">
-            <a href="<?php echo $base_url; ?>/index.php" class="nav-item nav-link century-gothic">CLAUDE &amp; THE PIGEON</a>
-            <a href="<?php echo $base_url; ?>/index.php" class="nav-item nav-link active">HOME</a>
-            <a href="<?php echo $base_url; ?>/about.php" class="nav-item nav-link active">ABOUT</a>
+            <a href="<?php echo $base_url; ?>/index" class="nav-item nav-link century-gothic">CLAUDE &amp; THE PIGEON</a>
+            <a href="<?php echo $base_url; ?>/index" class="nav-item nav-link active">HOME</a>
+            <a href="<?php echo $base_url; ?>/about" class="nav-item nav-link active">ABOUT</a>
             <!-- <a href="about.html" class="nav-item nav-link">About Us</a> -->
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">STOCK</a>
                 <div class="dropdown-menu border-0 rounded-0 rounded-bottom m-0">
-                    <a href="<?php echo $base_url; ?>/stock/category.php?c=<?php echo $seating; ?>" class="dropdown-item">Seating</a>
-                    <a href="<?php echo $base_url; ?>/stock/category.php?c=<?php echo $lighting; ?>" class="dropdown-item">Lighting</a>
-                    <a href="<?php echo $base_url; ?>/stock/category.php?c=<?php echo $tables; ?>" class="dropdown-item">Tables</a>
-                    <a href="<?php echo $base_url; ?>/stock/category.php?c=<?php echo $cabinets; ?>" class="dropdown-item">Cabinets & Storage</a>
-                    <a href="<?php echo $base_url; ?>/stock/category.php?c=<?php echo $mirrors; ?>" class="dropdown-item">Mirrors</a>
-                    <a href="<?php echo $base_url; ?>/stock/category.php?c=<?php echo $decorative; ?>" class="dropdown-item">Decorative</a>
+                    <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $seating; ?>" class="dropdown-item">Seating</a>
+                    <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $lighting; ?>" class="dropdown-item">Lighting</a>
+                    <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $tables; ?>" class="dropdown-item">Tables</a>
+                    <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $cabinets; ?>" class="dropdown-item">Cabinets & Storage</a>
+                    <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $mirrors; ?>" class="dropdown-item">Mirrors</a>
+                    <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $decorative; ?>" class="dropdown-item">Decorative</a>
                 </div>
             </div>
-            <a href="<?php echo $base_url; ?>/contact.php" class="nav-item nav-link">CONTACT US</a>
+            <a href="<?php echo $base_url; ?>/contact" class="nav-item nav-link">CONTACT US</a>
         </div>
     </div>
     <div class="col-4">
