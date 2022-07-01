361affa5f4d852959921948ae395a599a76e527f
Will Mason <willmason@live.com>
2022-07-01 16:09:43 +0100

    Update footer to remove fadein animation

--- sections/footer.php
+++ sections/footer.php
@@ -1,12 +1,12 @@
 
 <!-- Footer Start -->
-<div class="container-fluid bg-cream text-body footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+<div class="container-fluid bg-cream text-body footer mt-5 pt-5" data-wow-delay="0.1s">
     <div class="container py-5">
-    <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+    <div class="text-center mx-auto mb-5" data-wow-delay="0.1s" style="max-width: 600px;">
             <h6 class="section-title text-center text-black px-3">Contact Us</h6>
         </div>
         <div class="row g-0 justify-content-center">
-            <div class="col-lg-8 wow fadeInUp" data-wow-delay="0.5s">
+            <div class="col-lg-8" data-wow-delay="0.5s">
                 <form 
                 name="formContact" 
                 method="POST"
