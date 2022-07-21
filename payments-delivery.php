f9ba257dafc0d0592bfe111ecaa1b8b1f8b4e8cc
Will Mason <willmason@live.com>
2022-07-21 17:40:14 +0100

    Add payments & derlivery page

---
+++ payments-delivery.php
@@ -0,0 +1,135 @@
+<?php 
+
+require_once 'includes/mysql-connect-local.php';
+require_once 'includes/base-url.php';
+require_once 'includes/contact-form/process.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Claude &amp; the Pigeon Antiques</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <!-- <link href="favicon.ico" rel="icon"> -->
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="assets/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/slick-slider.css" rel="stylesheet">
+</head>
+
+<body>
+
+<?php require_once 'sections/navigation.php'; ?>
+
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto mb-5" data-wow-delay="0.1s" style="max-width: 600px;">
+                <h1 class="bg-white text-center text-primary">
+                Payment
+                </h1>
+
+                <p>
+          Once you have made an enquiry about one of our items we will contact you via email or telephone to discuss how you wish to receive the item. We able to accommodate either delivery or collect. Once arrangements have been made we will send an invoice for payment via bank transfer.
+
+                </p>
+                <h1 class="bg-white text-center text-primary">
+                Postage
+                </h1>
+
+                <p>
+We’re happy to work with you on postage, if you have a trusted courier then we simply ask for them to contact us to make arrangements. Alternatively we can offer a price for postage of smaller items through a standard courier or larger item delivery through one of our tried and trusted courier services.
+
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+	<?php require_once 'sections/footer.php'; ?>
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/lib/wow/wow.min.js"></script>
+    <script src="assets/lib/easing/easing.min.js"></script>
+    <script src="assets/lib/waypoints/waypoints.min.js"></script>
+    <script src="assets/lib/counterup/counterup.min.js"></script>
+    <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="assets/lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/slick-slider.js"></script>
+
+<script type="text/javascript">
+        function validateContactForm() {
+            var valid = true;
+
+            $(".info").html("");
+            $(".input-field").css('border', '#FF0000 4px solid');
+            var userName = $("#name").val();
+            var userEmail = $("#email").val();
+            var subject = $("#subject").val();
+            var message = $("#message").val();
+            
+            if (userName == "") {
+                $("#name-info").html("Required.");
+                $("#userName").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (userEmail == "") {
+                $("#email-info").html("Required.");
+                $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (!userEmail.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/))
+            {
+                $("#email-info").html("Invalid Email Address.");
+                $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+
+            if (subject == "") {
+                $("#subject-info").html("Required.");
+                $("#subject").css('border', '#FF0000 4px solid');
+                valid = false;
+            }
+            if (message == "") {
+                $("#message-info").html("Required.");
+                $("#content").css('border', '#FF0000 4px solid');
+                valid = false;
+            }
+            return valid;
+        }
+</script>
+</body>
+
+</html>
--- sections/navigation.php
+++ sections/navigation.php
@@ -14,7 +14,9 @@ $decorative = 6;
     <i class="fa fa-laptop-code fa-2x text-primary position-absolute top-50 start-50 translate-middle"></i>
 </div>
 <!-- Spinner End -->
+<style>
 
+</style>
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-primary navbar-light sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
     <a href="<?php echo $base_url; ?>" class="navbar-brand ms-3 d-lg-none century-gothic">CLAUDE &amp; THE PIGEON</a>
@@ -24,13 +26,31 @@ $decorative = 6;
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav me-auto p-3 p-lg-0">
             <a href="<?php echo $base_url; ?>/index" class="nav-item nav-link century-gothic">CLAUDE &amp; THE PIGEON</a>
-            <a href="<?php echo $base_url; ?>/index" class="nav-item nav-link active">HOME</a>
-            <a href="<?php echo $base_url; ?>/about" class="nav-item nav-link active">ABOUT</a>
+            <a  href="<?php echo $base_url; ?>/index" 
+                class="nav-item nav-link 
+                <?php 
+                if (strpos($_SERVER['REQUEST_URI'], "index") !== false){
+                 echo "active";
+                }
+                ?>
+                ">
+                HOME
+            </a>
+            <a href="<?php echo $base_url; ?>/about" class="nav-item nav-link">ABOUT</a>
             <!-- <a href="about.html" class="nav-item nav-link">About Us</a> -->
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">STOCK</a>
                 <div class="dropdown-menu border-0 rounded-0 rounded-bottom m-0">
-                    <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $seating; ?>" class="dropdown-item">Seating</a>
+                    <a  href="<?php echo $base_url; ?>/stock/category?c=<?php echo $seating; ?>" 
+                        class="dropdown-item 
+                        <?php 
+                        if (strpos($_SERVER['REQUEST_URI'], "stock") !== false){
+                         echo "active";
+                        }
+                        ?>
+                        ">
+                    Seating
+                    </a>
                     <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $lighting; ?>" class="dropdown-item">Lighting</a>
                     <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $tables; ?>" class="dropdown-item">Tables</a>
                     <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $cabinets; ?>" class="dropdown-item">Cabinets & Storage</a>
@@ -38,10 +58,16 @@ $decorative = 6;
                     <a href="<?php echo $base_url; ?>/stock/category?c=<?php echo $decorative; ?>" class="dropdown-item">Decorative</a>
                 </div>
             </div>
+            <div class="nav-item dropdown">
+                <a href="<?php echo $base_url; ?>/payments-delivery" class="nav-link dropdown-toggle">PAYMENTS &amp; DELIVERY</a>
+                <div class="dropdown-menu border-0 rounded-0 rounded-bottom m-0">
+                    <a href="<?php echo $base_url; ?>/returns" class="dropdown-item">Returns</a>
+                </div>
+            </div>
             <a href="<?php echo $base_url; ?>/contact" class="nav-item nav-link">CONTACT US</a>
         </div>
     </div>
-    <div class="col-4">
+    <div class="col-1">
         <div class="d-flex align-items-center justify-content-end">
              <div style="display: inline-flex;">
                 <a 
